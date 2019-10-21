34385649cd6c30a562e3763489beb9856b158a8d
Elvin Ciqueira <elvinciqueira@gmail.com>
2019-10-21 18:55:06 -0300

    config sequelize, model, migration e controller

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
-import app from 'express';
+import express from 'express';
+import routes from './routes';
 
-app.listen(3333);
+import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
-const routes = Router();
+import UserController from './app/controllers/UserController';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
 
 export default routes;
